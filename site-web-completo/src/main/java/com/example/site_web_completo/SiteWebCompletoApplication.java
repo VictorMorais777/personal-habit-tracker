398806c5e5525ad9df7718cec40e627a905baf65
Victor <joaovictrr6@gmail.com>
2026-09-12 22:09:43 -0300

    Primeiro commit - Criação do Projeto

---
+++ site-web-completo/src/main/java/com/example/site_web_completo/SiteWebCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.example.site_web_completo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteWebCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteWebCompletoApplication.class, args);
+	}
+
+}
---
+++ site-web-completo/src/test/java/com/example/site_web_completo/SiteWebCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.site_web_completo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteWebCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

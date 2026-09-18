c29a51c6559411798dafbb88acf8825872459d37
Victor <joaovictrr6@gmail.com>
2026-09-18 18:07:59 -0300

    Criação da classe UserController

---
+++ site-web-completo/src/main/java/com/example/site_web_completo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.site_web_completo.controller;
+
+import com.example.site_web_completo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+}

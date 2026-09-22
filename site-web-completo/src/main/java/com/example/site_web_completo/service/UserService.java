a0cc21846522f2dfaaeff5381669c27d28edc8c5
Victor <joaovictrr6@gmail.com>
2026-09-21 21:42:42 -0300

    Criação da classe UserService

---
+++ site-web-completo/src/main/java/com/example/site_web_completo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.site_web_completo.service;
+
+import com.example.site_web_completo.model.User;
+import com.example.site_web_completo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
